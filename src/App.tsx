b577246dc903344608d73cc637de47652b1301bf
MarkSav1 <mns99@windowslive.com>
2024-04-30 00:20:00 +0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import AuthLayout from './_auth/AuthLayout';
+import SigninForms from './_auth/forms/SigninForms';
+import SignupForm from './_auth/forms/SignupForm';
+import { Home } from './_root';
+import RootLayout from './_root/RootLayout';
+import '/src/globals.css';
+import { Routes, Route } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <main className='flex h-screem'>
+      <Routes>
+        {/* Public Routes */}  
+        <Route element={<AuthLayout />}>
+        <Route path='/sign-in' element={<SigninForms />} />
+        <Route path='/sign-up' element={<SignupForm />} />
+        </Route>
+        
+        {/* Private Routes */}
+        <Route element={<RootLayout />}>
+        <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  )
+}
+
+export default App
---
+++ src/_root/RootLayout.tsx
@@ -0,0 +1,7 @@
+const RootLayout = () => {
+  return (
+    <div className="underline">RootLayout</div>
+  )
+}
+
+export default RootLayout
